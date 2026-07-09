307d6d6a6e6ff35bc5082c796c6cf0ea57b3a034
Avisha1204 <agrawal.avisha04@gmail.com>
2026-07-09 13:03:13 -0700

    Create DriveBaseSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/DriveBaseSubsystem.java
@@ -0,0 +1,98 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveBaseSubsystem extends SubsystemBase {
+  private SparkMax motortopleft = null;
+  private SparkMax motortopright = null;
+  private SparkMax motorbottomleft = null;
+  private SparkMax motorbottomright = null;
+  private final DifferentialDrive drive;
+  /** Creates a new ExampleSubsystem. */
+  public DriveBaseSubsystem(DifferentialDrive drive) {
+    motortopleft = new SparkMax(20, MotorType.kBrushless);
+    motortopright = new SparkMax(21, MotorType.kBrushless);
+    motorbottomleft = new SparkMax(178, MotorType.kBrushless);
+    motorbottomright = new SparkMax(56, MotorType.kBrushless);
+
+
+    this.drive = drive;
+  }
+
+  public DriveBaseSubsystem(){
+    this.drive = null;
+    SparkMaxConfig motortopleftconfig = new SparkMaxConfig();
+    SparkMaxConfig motortoprightconfig = new SparkMaxConfig();
+    SparkMaxConfig motorbottomleftconfig = new SparkMaxConfig();
+    SparkMaxConfig motorbottomrightconfig = new SparkMaxConfig();
+
+    motorbottomleftconfig.smartCurrentLimit(40);
+    motortopleftconfig.smartCurrentLimit(40);
+    motorbottomrightconfig.smartCurrentLimit(40);
+    motorbottomleftconfig.smartCurrentLimit(40);
+
+    motortopleftconfig.inverted(false);
+    motortoprightconfig.inverted(true);
+
+    motorbottomleftconfig.follow(motortopleft, true);
+    motorbottomrightconfig.follow(motortopright, true);
+
+
+   
+    
+    
+    SparkBaseConfig config;
+    motorbottomleft.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+    motortopleft.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+    motorbottomright.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+    motortopright.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command DriveBaseMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
