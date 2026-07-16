e6ab3de3109ff077c0f40104d434351840f6852f
Avisha1204 <agrawal.avisha04@gmail.com>
2026-07-16 15:47:55 -0700

    new stuff with auto

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -2,19 +2,27 @@
 // // Open Source Software; you can modify and/or share it under the terms of
 // // the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import edu.wpi.first.wpilibj2.command.Command;
-// import edu.wpi.first.wpilibj2.command.Commands;
-// import frc.robot.subsystems.ShooterSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.DriveBaseSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
-// public final class Autos {
-//   /** Example static factory for an autonomous command. */
-//   public static Command exampleAuto(ShooterSubsystem subsystem) {
-//     return Commands.sequence(subsystem.shooterMethodCommand(), new ExampleCommand(subsystem));
-//   }
+public final class Autos {
+  /** Example static factory for an autonomous command. */
+  public static Command moveForwardAndShoot(ShooterSubsystem shooterSubsystem, DriveBaseSubsystem driveBaseSubsystem) {
+    return Commands.sequence(
+      new DriveCommands(driveBaseSubsystem, () -> -1, () -> 0),
+      Commands.waitSeconds(2),
+      new DriveCommands(driveBaseSubsystem, () -> 0, () -> 0),
+      new ShooterCommands(shooterSubsystem),
+      Commands.waitSeconds(5),
+      new ShooterStopCommands(shooterSubsystem)
+  );
+  }
 
-//   private Autos() {
-//     throw new UnsupportedOperationException("This is a utility class!");
-//   }
-// }
+  private Autos() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
